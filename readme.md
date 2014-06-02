40a37e82c15993a67e527a86a6a74dcf2c1512aa
Rob Ellis <rob@chloi.io>
2014-06-01 19:00:20 -0700

    Adds questionType and isQuestion to interface

--- readme.md
+++ readme.md
@@ -16,6 +16,14 @@ Using Trec 10 Labled questions from : http://cogcomp.cs.illinois.edu/Data/QA/QC/
     classify("How many steps from here to Alaska?"); 
     // NUM:count
 
+    questionType("How many steps from here to Alaska?"); 
+    // WH
+    Returns 
+    	- CH: Choice or Alternate Question
+      - WH: QWord Question
+      - YN: Yes/No Question
+      - TG: Tag Question
+
     assert("file", callback)
 
 See the test file for an example.
@@ -23,6 +31,6 @@ See the test file for an example.
 Results in sample questions are roughtly 
 80+% on course features and 75% on finer sub categories
 
-    { couse: 84.90945674044266, fine: 76.45875251509054 }
+    { coarse: 80.19603920784157, fine: 71.37427485497099 }
 
  
--- test/basic.js
+++ test/basic.js
@@ -2,9 +2,29 @@ var ra = require("../lib/ruleClassify.js");
 
 new ra(function(q) {
 	
-	q.classify("How many steps from here to Alaska?");
+	// Question Word Questions
+	var r1 = q.questionType("How many steps from here to Alaska?");
+	
+	// Alternative question or Choice Questions
+	// http://aclweb.org/anthology/Y/Y00/Y00-1037.pdf
+	var r2 = q.questionType("Who is slower, Mary or John?");
+	
+	// Tag Questions
+	// var r3 = q.questionType("She does a beautiful job, does not she?");
+	var r3 = q.questionType("Peter plays football, does not he?");
 
-	q.assert("./data/500q.txt", function(results) {
-		console.log(results);
-	});
+	// YN Questions
+	var r4 = q.questionType("Do you want dinner?");
+
+	// q.assertAlt("./data/altQuestions.txt", function(results) {
+	// 	console.log(results);
+	// });
+
+	// q.assertTag("./data/tagQuestions.txt", function(results) {
+	// 	console.log(results);
+	// });
+
+	// q.assert("./data/500q.txt", function(results) {
+	// 	console.log(results);
+	// });
 });
